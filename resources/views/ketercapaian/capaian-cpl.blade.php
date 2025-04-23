c4175918b2d7bd9a89dc21dc03bee86f9cb93ca6
Intan Budiarty <intanbudiarty03@gmail.com>
2025-04-23 16:10:51 +0700

    feat:menambahkan kolom ttd kaprodi pada cetak CPL

--- resources/views/ketercapaian/capaian-cpl.blade.php
+++ resources/views/ketercapaian/capaian-cpl.blade.php
@@ -10,7 +10,7 @@
                         <td>{{ $mahasiswa->nama }}</td>
                     </tr>
                     <tr>
-                        <td><strong>NIM: </strong></td>
+                        <td><strong>NIM:</strong></td>
                         <td>{{ $mahasiswa->nim }}</td>
                     </tr>
                     <tr>
@@ -22,47 +22,52 @@
             <div class="col">
                 <table class="table table-bordered">
                     <tr>
-                        <td><strong>Fakultas: </strong></td>
+                        <td><strong>Fakultas:</strong></td>
                         <td>TEKNIK</td>
                     </tr>
                     <tr>
-                        <td><strong>Program Studi: </strong></td>
+                        <td><strong>Program Studi:</strong></td>
                         <td>SISTEM INFORMASI</td>
                     </tr>
-                    {{-- <tr>
-                        <td><strong>Angkatan:</strong></td>
-                        <td>{{ $mahasiswa->angkatan }}</td>
-                    </tr> --}}
                 </table>
             </div>
-            <div class="row-md-8">
-                <table class="table table-bordered">
-                    <thead>
+        </div> {{-- end row --}}
+        
+        {{-- Tabel CPL --}}
+        <div class="mt-4">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Kode CPL</th>
+                        <th>Nama CPL</th>
+                        <th>Total Nilai</th>
+                        <th>Total Skor Maksimal</th>
+                        <th>Persentase (%)</th>
+                        <th>Predikat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($capaianCpl as $cpl)
                         <tr>
-                            <th>Kode CPL</th>
-                            <th>Nama CPL</th>
-                            <th>Total Nilai</th>
-                            <th>Total Skor Maksimal</th>
-                            <th>Persentase (%)</th>
-                            <th>Predikat</th>
+                            <td>{{ $cpl['kode_cpl'] }}</td>
+                            <td>{{ $cpl['nama_cpl'] }}</td>
+                            <td>{{ $cpl['total_nilai'] }}</td>
+                            <td>{{ $cpl['total_skor_maksimal'] }}</td>
+                            <td>{{ $cpl['persentase'] }}%</td>
+                            <td>{{ $cpl['predikat'] }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($capaianCpl as $cpl)
-                            <tr>
-                                <td>{{ $cpl['kode_cpl'] }}</td>
-                                <td>{{ $cpl['nama_cpl'] }}</td>
-                                <td>{{ $cpl['total_nilai'] }}</td>
-                                <td>{{ $cpl['total_skor_maksimal'] }}</td>
-                                <td>{{ $cpl['persentase'] }}%</td>
-                                <td>{{ $cpl['predikat'] }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        {{-- Tanda Tangan --}}
+        <div style="margin-left: 80px; text-align: end;">
+            <p>Mengetahui,</p>
+            <p>Kepala Program Studi Sistem Informasi</p>
+            <br><br><br>
+            <p style="font-weight: bold; text-decoration: underline;">Dr.Endina Putri Purwandari S.T.,M.KOM</p>
         </div>
     </div>
 </div>
-    
 @endsection
