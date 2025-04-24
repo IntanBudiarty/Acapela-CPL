12a66b6f17620072625b18dc3f29bb8548afbe65
Intan Budiarty <intanbudiarty03@gmail.com>
2025-04-24 12:44:30 +0700

    Feat: menambahkan Kop surat

--- resources/views/ketercapaian/capaian-cpl.blade.php
+++ resources/views/ketercapaian/capaian-cpl.blade.php
@@ -2,12 +2,21 @@
 @section('content')
 <div class="card">
     <div class="card-body">
-        <div class="row">
-            <div class="container">
-                <img src="{{ asset('media/photos/ketercapaian/Logo-unib.png') }}" alt="" width="100" height="100">
+
+        {{-- Kop Surat --}}
+        <div class="row align-items-center mb-4">
+            <div class="col-2 text-center">
+                <img src="{{ asset('media/photos/ketercapaian/Logo-unib.png') }}" alt="Logo UNIB" width="100" height="100">
+            </div>
+            <div class="col-10 text-right">
+                <h4 style="margin:0;">UNIVERSITAS BENGKULU</h4>
+                <h6 style="margin:0;">FAKULTAS TEKNIK</h6>
+                <h6 style="margin:0;">PROGRAM STUDI SISTEM INFORMASI</h6>
+                {{-- <hr style="border: 1px solid #000;"> --}}
             </div>
         </div>
-        <br>
+
+        {{-- Data Mahasiswa --}}
         <div class="row">
             <div class="col">
                 <table class="table table-bordered">
@@ -16,7 +25,7 @@
                         <td>{{ $mahasiswa->nama }}</td>
                     </tr>
                     <tr>
-                        <td><strong>NIM: </strong></td>
+                        <td><strong>NIM:</strong></td>
                         <td>{{ $mahasiswa->nim }}</td>
                     </tr>
                     <tr>
@@ -28,53 +37,51 @@
             <div class="col">
                 <table class="table table-bordered">
                     <tr>
-                        <td><strong>Fakultas: </strong></td>
+                        <td><strong>Fakultas:</strong></td>
                         <td>TEKNIK</td>
                     </tr>
                     <tr>
-                        <td><strong>Program Studi: </strong></td>
+                        <td><strong>Program Studi:</strong></td>
                         <td>SISTEM INFORMASI</td>
                     </tr>
-                    {{-- <tr>
-                        <td><strong>Angkatan:</strong></td>
-                        <td>{{ $mahasiswa->angkatan }}</td>
-                    </tr> --}}
                 </table>
             </div>
-            <div class="row-md-8">
-                <table class="table table-bordered">
-                    <thead>
+        </div>
+
+        {{-- Tabel CPL --}}
+        <div class="mt-4">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Kode CPL</th>
+                        <th>Nama CPL</th>
+                        <th>Total Nilai</th>
+                        <th>Total Skor Maksimal</th>
+                        <th>Persentase (%)</th>
+                        <th>Predikat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($capaianCpl as $cpl)
                         <tr>
-                            <th>Kode CPL</th>
-                            <th>Nama CPL</th>
-                            <th>Total Nilai</th>
-                            <th>Total Skor Maksimal</th>
-                            <th>Persentase (%)</th>
-                            <th>Predikat</th>
+                            <td>{{ $cpl['kode_cpl'] }}</td>
+                            <td>{{ $cpl['nama_cpl'] }}</td>
+                            <td>{{ $cpl['total_nilai'] }}</td>
+                            <td>{{ $cpl['total_skor_maksimal'] }}</td>
+                            <td>{{ $cpl['persentase'] }}%</td>
+                            <td>{{ $cpl['predikat'] }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($capaianCpl as $cpl)
-                            <tr>
-                                <td>{{ $cpl['kode_cpl'] }}</td>
-                                <td>{{ $cpl['nama_cpl'] }}</td>
-                                <td>{{ $cpl['total_nilai'] }}</td>
-                                <td>{{ $cpl['total_skor_maksimal'] }}</td>
-                                <td>{{ $cpl['persentase'] }}%</td>
-                                <td>{{ $cpl['predikat'] }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <div style="margin-left: 80px; text-align: end;">
-                    <p>Mengetahui,</p>
-                    <p>Kepala Program Studi Sistem Informasi</p>
-                    <br><br><br>
-                    <p style="font-weight: bold; text-decoration: underline;">Dr.Endina Putri Purwandari S.T.,M.KOM</p>
-                </div>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div style="margin-left: 80px; text-align: end;">
+            <p>Mengetahui,</p>
+            <p>Kepala Program Studi Sistem Informasi</p>
+            <br><br><br>
+            <p style="font-weight: bold; text-decoration: underline;">Dr.Endina Putri Purwandari S.T.,M.KOM</p>
         </div>
     </div>
 </div>
-    
 @endsection
